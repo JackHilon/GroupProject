52297b8ba60086db1975a818506ca567ced200e3
JackHilon <markandmark1111@gmail.com>
2019-12-11 01:10:47 -0800

    string with number

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -6,7 +6,9 @@ namespace GroupProject
     {
         static void Main(string[] args)
         {
+            var str = "We are all!";
             Console.WriteLine("Hello Oliver!");
+            Console.WriteLine($"str {3 + 5} .....!");
         }
     }
 }
