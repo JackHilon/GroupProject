31d639f14fe2a378c769179a2e7615aa62652e73
OlSarLang <o.sarholm@hotmail.com>
2019-12-11 10:31:46 +0100

    Added several Oliver lines

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -8,6 +8,9 @@ namespace GroupProject
         {
             var str = "We are all!";
             Console.WriteLine("Hello Oliver!");
+            Console.WriteLine("Hello Oliver!");
+            Console.WriteLine("Hello Oliver!");
+            Console.WriteLine("Hello Oliver!");
             Console.WriteLine($"str {3 + 5} .....!");
             Console.WriteLine("Hello every one!");
             for(int i = 0; i < 100; i++){
