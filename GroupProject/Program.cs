a2cf027ab6d28b88857f55103d8aebe44e66a763
OlSarLang <o.sarholm@hotmail.com>
2019-12-11 10:09:45 +0100

    Loop

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -7,6 +7,9 @@ namespace GroupProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Jack!");
+            for(int i = 0; i < 100; i++){
+                Console.WriteLine(i.ToString());
+            }
         }
     }
 }
