b65bf57afa7208f78eb9149f954ae60780d93683
OlSarLang <o.sarholm@hotmail.com>
2019-12-11 10:00:57 +0100

    Changed Hello World to Hello Jack

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -6,7 +6,7 @@ namespace GroupProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Jack!");
         }
     }
 }
